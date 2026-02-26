13fcf2fdefcecc190b9b25d11ee464285d68e1cc
Richardbrown85 <richard.s.brown@live.co.uk>
2026-02-26 22:03:07 +0000

    fix: remove STATICFILES_DIRS for proper app-level static file discovery

--- cinemai_project/settings.py
+++ cinemai_project/settings.py
@@ -100,7 +100,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'cinemai' / 'static']
 
 # WhiteNoise will serve static files
 STORAGES = {
