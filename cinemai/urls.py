e5546a7ace20d0ade284ca29c64220de4277c59b
Richardbrown85 <richard.s.brown@live.co.uk>
2026-03-05 21:30:23 +0000

    Reset emails are being sent to terminal now to test when deployed to Heroku

--- cinemai/urls.py
+++ cinemai/urls.py
@@ -16,13 +16,13 @@ urlpatterns = [
          views.CustomPasswordResetView.as_view(), 
          name='password_reset'),
     path('password-reset/done/', 
-         auth_views.PasswordResetDoneView.as_view(template_name='cinema/password_reset_done.html'), 
+         auth_views.PasswordResetDoneView.as_view(template_name='cinemai/password_reset_done.html'), 
          name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', 
          views.CustomPasswordResetConfirmView.as_view(), 
          name='password_reset_confirm'),
     path('password-reset-complete/', 
-         auth_views.PasswordResetCompleteView.as_view(template_name='cinema/password_reset_complete.html'), 
+         auth_views.PasswordResetCompleteView.as_view(template_name='cinemai/password_reset_complete.html'), 
          name='password_reset_complete'),
     
     # Account Management
